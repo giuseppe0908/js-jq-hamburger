8802b2dc1e85f736ebcbe0d0e4fa607e79ae336e
giuseppe <placidagiuseppe@gmail.com>
2021-03-11 11:04:07 +0100

    esercizio terminato

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //far comparire hamburgher anche in versione lg
-var hamburger = $(".fa-bars");
+var hamburger = $(".header-right a i.fa-bars");
 var hamburger_menu = $(".hamburger-menu");
 
   hamburger.click(function(){
-    hamburger_menu.toggle("active");
+    hamburger_menu.addClass("active");
 });
-var chiusura = $(".fa-times");
+var chiusura = $(".hamburger-menu .close > i.fa-times");
  chiusura.click(function(){
-     hamburger_menu.toggle("active");
+     hamburger_menu.removeClass("active");
 });
